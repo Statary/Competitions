9db0b1a6f272cdb6954d18fdadaf1796be9829e3
Statary <tatarinov_stas@mail.ru>
2014-09-25 04:46:56 +0400

    jtable added

--- src/competitions/Competitions.java
+++ src/competitions/Competitions.java
@@ -6,6 +6,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.table.TableModel;
 
 public class Competitions {
 
@@ -18,21 +23,35 @@ public class Competitions {
 
         try {
             conn = MySqliteConn.getConnection();
-            String sql = "select * from commands;";
-            statement = conn.createStatement();
-            res = statement.executeQuery(sql);
-            while (res.next()){
-                System.out.println(res.getObject("name"));
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(Competitions.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-              if (statement != null) statement.close();
-              if (res != null) res.close();
-            } catch (SQLException ex) {
-              Logger.getLogger(Competitions.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            TableModel mod = new MyTableModel(conn, "commands");
+            
+            JTable jtable = new JTable(mod);
+            jtable.setDefaultRenderer(Object.class, new MyTableRenderer());
+            JScrollPane scroll = new JScrollPane(jtable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+            
+            JFrame frame = new JFrame("Competitions");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.getContentPane().add(scroll);
+            frame.pack();
+            frame.setVisible(true);
+            
+            conn.close();
+                    
+//            String sql = "select * from commands;";
+//            statement = conn.createStatement();
+//            res = statement.executeQuery(sql);
+//            while (res.next()){
+//                System.out.println(res.getObject("name"));
+//            }
+        } catch (Exception e) {
+            e.printStackTrace();
+//        } finally {
+//            try {
+//              if (statement != null) statement.close();
+//              if (res != null) res.close();
+//            } catch (SQLException ex) {
+//              Logger.getLogger(Competitions.class.getName()).log(Level.SEVERE, null, ex);
+//            }
         }
     }
 }
---
+++ src/competitions/MyTableModel.java
@@ -0,0 +1,183 @@
+package competitions;
+
+import javax.swing.table.*;
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.event.TableModelEvent;
+
+public class MyTableModel extends AbstractTableModel {
+    
+    private static Connection conn;
+
+    private Object[][] contents;
+    private String[] columnNames;
+    private Class[] columnClasses;
+
+    public MyTableModel(Connection conn,
+            String tableName)
+            throws SQLException {
+        super();
+        MyTableModel.conn = conn;
+        getTableContents(tableName);
+    }
+
+    private void getTableContents(String tableName)
+            throws SQLException {
+
+        DatabaseMetaData meta = conn.getMetaData();
+
+        ResultSet rs = meta.getColumns(null, null, tableName, null);
+
+        ArrayList colNamesList = new ArrayList();
+        ArrayList colTypesList = new ArrayList();
+
+        while (rs.next()) {
+            colNamesList.add(rs.getString("COLUMN_NAME"));
+            int dbType = rs.getInt("DATA_TYPE");
+
+            switch (dbType) {
+                case Types.INTEGER:
+                    colTypesList.add(Integer.class);
+                    break;
+                case Types.FLOAT:
+                    colTypesList.add(Float.class);
+                    break;
+                case Types.DOUBLE:
+                case Types.REAL:
+                    colTypesList.add(Double.class);
+                    break;
+                case Types.DATE:
+                case Types.TIME:
+                case Types.TIMESTAMP:
+                    colTypesList.add(java.sql.Date.class);
+                    break;
+                default:
+                    colTypesList.add(String.class);
+                    break;
+            };
+        }
+
+        columnNames = new String[colNamesList.size()];
+        colNamesList.toArray(columnNames);
+
+        columnClasses = new Class[colTypesList.size()];
+        colTypesList.toArray(columnClasses);
+
+        Statement statement = conn.createStatement();
+        rs = statement.executeQuery("SELECT * FROM " + tableName);
+        ArrayList rowList = new ArrayList();
+
+        while (rs.next()) {
+            ArrayList cellList = new ArrayList();
+            for (int i = 0; i < columnClasses.length; i++) {
+                Object cellValue = null;
+
+                //TODO: pattern should be here
+                if (columnClasses[i] == String.class) { cellValue = rs.getString(columnNames[i]);
+                } else if (columnClasses[i] == Integer.class) { cellValue = new Integer(rs.getInt(columnNames[i]));
+                } else if (columnClasses[i] == Float.class) { cellValue = new Float(rs.getInt(columnNames[i]));
+                } else if (columnClasses[i] == Double.class) { cellValue = new Double(rs.getDouble(columnNames[i]));
+                } else if (columnClasses[i] == java.sql.Date.class) { cellValue = rs.getDate(columnNames[i]);
+                } else {System.out.println("Undefined type " + columnNames[i]);
+                }
+
+                cellList.add(cellValue);
+            }
+
+            Object[] cells = cellList.toArray();
+            rowList.add(cells);
+        }
+
+        contents = new Object[rowList.size()][];
+        for (int i = 0; i < contents.length; i++) {
+            contents[i] = (Object[]) rowList.get(i);
+        }
+        
+        if (rs!=null ) rs.close();
+        if (statement!=null ) statement.close();
+    }
+    
+    
+//     public boolean updateDB(String tableName) {
+//         
+//        ArrayList<String> sqlList = new ArrayList();
+//        
+//        for (int i = 0; i < contents.length; i++) {
+//            Object[] objects = contents[i];
+//            sqlList.add("update " + tableName + " set name_ru='" + objects[1] + "', name_en='" + objects[2] + "' where id=" + objects[0] + ";");
+//        }
+//
+//        Statement statement = null;
+//        
+//        try {
+//            statement = conn.createStatement();
+//            
+//            for (String sql : sqlList) {
+//                statement.executeUpdate(sql);
+//            }
+//            
+//        } catch (SQLException ex) {
+//            Logger.getLogger(MyTableModel.class.getName()).log(Level.SEVERE, null, ex);
+//            return false;
+//        }finally{
+//            try {
+//                if (statement!=null) statement.close();
+//            } catch (SQLException ex) {
+//                Logger.getLogger(MyTableModel.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }
+//        
+//        return true;
+//
+//    }
+     
+     
+
+    @Override
+    public int getRowCount() {
+        return contents.length;
+    }
+
+    @Override
+    public int getColumnCount() {
+        if (contents.length == 0) {
+            return 0;
+        } else {
+            return contents[0].length;
+        }
+    }
+
+    @Override
+    public Object getValueAt(int row, int column) {
+        return contents[row][column];
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        contents[rowIndex][columnIndex] = aValue;
+
+        fireTableCellUpdated(rowIndex, columnIndex);
+    }
+
+    @Override
+    public Class getColumnClass(int col) {
+        return columnClasses[col];
+    }
+
+    @Override
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        if (columnIndex == 0) {
+            return false;
+        }
+        return true;
+    }
+
+   
+}
